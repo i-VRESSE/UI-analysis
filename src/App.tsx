9258d709378d9dd6fbf419f912efe22379908e59
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-07-19 13:49:29 +0200

    remove extra semicolon after table

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { stat_labels, clusters } from "./examples/TableData";
 const App = () => {
   return (
     <div>
-      <ClusterTable stat_labels={stat_labels} clusters={clusters} />;
+      <ClusterTable stat_labels={stat_labels} clusters={clusters} />
     </div>
   );
 };
