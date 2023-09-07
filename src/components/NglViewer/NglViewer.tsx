65bbaff36d8abb028c3860cd8c21c51837615921
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-09-07 15:57:11 +0200

    run npm format

--- src/components/NglViewer/NglViewer.tsx
+++ src/components/NglViewer/NglViewer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import {Stage, type Component} from "ngl";
+import { Stage, type Component } from "ngl";
 import "./NglViewer.css";
 interface ActiveStructure {
   fileName: string;
