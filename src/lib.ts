e2ea345914550c0edd3b1ce8a70eacaa28920fee
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-01-30 12:37:04 +0100

    Format

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,3 @@
 import { ClusterTable } from "./components/ClusterTable";
 import { StructureTable } from "./components/StructureTable";
-export { ClusterTable, StructureTable }
+export { ClusterTable, StructureTable };
